3ebcd0ed79ba75ddcb930ee66b12090610a5cb9a
alex0207s <alexliao-dev@proton.me>
2024-02-29 19:42:18 +0800

    add hw2 template

---
+++ w1/script/MyToken.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {MyToken} from "src/MyToken.sol";
+
+contract MyTokenScript is Script {
+    MyToken myToken;
+
+    function setUp() public {}
+
+    function run() public {}
+}
+
