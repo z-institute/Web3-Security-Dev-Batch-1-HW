54b5cc523052cfb5b8f42f8b5331916dc8b10f29
alex0207s <alexliao-dev@proton.me>
2024-03-02 23:49:57 +0800

    add hw1 solution

--- w1/src/MyToken.sol
+++ w1/src/MyToken.sol
@@ -5,10 +5,28 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 pragma solidity 0.8.20;
 
 contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+    address public owner;
+    mapping(address account => uint256 amount) public mintedTokenAmount;
+
+    error InvalidAmount();
+
+    constructor() ERC20("MyToken", "MTK") {
+        owner = msg.sender;
+    }
 
     // implement a public mint function
     // it allows anyone, except the developer, to mint 10 tokens for themselves,
     // while the developer is not restricted by the limitation of minting only 10 tokens.
-    function mint(address to, uint256 amount) public returns (uint256) {}
+    function mint(address to, uint256 amount) public returns (uint256) {
+        mintedTokenAmount[msg.sender] += amount;
+
+        if (msg.sender == owner) {
+            _mint(to, amount);
+        } else {
+            if (mintedTokenAmount[msg.sender] > 10 ether) revert InvalidAmount();
+            _mint(to, amount);
+        }
+
+        return amount;
+    }
 }
--- w1/test/MyToken.t.sol
+++ w1/test/MyToken.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {MyToken} from "src/MyToken.sol";
 
 contract MyTokenTest is Test {
@@ -11,7 +11,76 @@ contract MyTokenTest is Test {
     address user2 = makeAddr("user2");
     address user3 = makeAddr("user3");
 
-    function setUp() public {}
+    function setUp() public {
+        vm.startPrank(deployer);
+        myToken = new MyToken();
+        vm.stopPrank();
+    }
 
-    function testTransferAcrossMultipleEOA() external {}
+    function testTransferAcrossMultipleEOA() external {
+        // mint token first
+        vm.startPrank(deployer);
+        myToken.mint(deployer, 100 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(deployer), 100 ether);
+
+        vm.startPrank(user1);
+        myToken.mint(user1, 10 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(user1), 10 ether);
+
+        vm.startPrank(user2);
+        myToken.mint(user2, 9 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(user2), 9 ether);
+
+        vm.startPrank(user3);
+        myToken.mint(user3, 8 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(user3), 8 ether);
+
+        // transfer tokens across eoa
+        vm.startPrank(user1);
+        myToken.transfer(user2, 5 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(user1), 5 ether);
+        assertEq(myToken.balanceOf(user2), 14 ether);
+
+        vm.startPrank(user2);
+        myToken.transfer(user3, 4 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(user2), 10 ether);
+        assertEq(myToken.balanceOf(user3), 12 ether);
+
+        vm.startPrank(user3);
+        myToken.transfer(user1, 3 ether);
+        vm.stopPrank();
+
+        assertEq(myToken.balanceOf(user1), 8 ether);
+        assertEq(myToken.balanceOf(user3), 9 ether);
+
+        vm.startPrank(user1);
+        myToken.approve(user2, 3 ether);
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        myToken.transferFrom(user1, user3, 2 ether);
+
+        assertEq(myToken.balanceOf(user1), 6 ether);
+        assertEq(myToken.balanceOf(user3), 11 ether);
+
+        uint256 allowance = myToken.allowance(user1, user2);
+        myToken.transferFrom(user1, user2, allowance);
+
+        assertEq(myToken.balanceOf(user1), 5 ether);
+        assertEq(myToken.balanceOf(user2), 11 ether);
+
+        vm.stopPrank();
+    }
 }
