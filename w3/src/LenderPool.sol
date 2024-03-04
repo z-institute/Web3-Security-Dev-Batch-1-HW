5774b783cdd5fc32323a77855250fdf48a824f0f
0xRory <0x1rory@gmail.com>
2024-03-04 08:49:57 +0800

    feat: change
    * solidity ^0.8.20
    * change ReentrancyGuard the openzeppelin url

--- w3/src/LenderPool.sol
+++ w3/src/LenderPool.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 interface IReceiver {
     function receiveTokens(address tokenAddress, uint256 amount) external;
