d18794cf70ae1626055b591b47dd76e10e9a31d1
0xRory <0x1rory@gmail.com>
2024-03-06 00:59:21 +0800

    feat: HW3 Done for now

--- w3/src/LenderPool.sol
+++ w3/src/LenderPool.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "forge-std/console.sol";
 
 interface IReceiver {
     function receiveTokens(address tokenAddress, uint256 amount) external;
