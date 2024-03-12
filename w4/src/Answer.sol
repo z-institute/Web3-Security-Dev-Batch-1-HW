8a5c8bd9f80d76fb45f3397d62364ffaa2705898
J <ab12366945@gmail.com>
2024-03-12 17:19:55 +0800

    update file

--- w4/src/Answer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-contract Answer {
-    function answer() public view returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp)));
-    }
-}
