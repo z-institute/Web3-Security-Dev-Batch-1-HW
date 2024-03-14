1159453dd4e8b8799941ed8607fbc384e8c921cc
memoriaXII <chenglei.chou@gmail.com>
2024-03-14 12:52:46 +0800

    Delete unnecessary files and configurations

--- w2/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const ZERO_ADDRESS: string = "0x0000000000000000000000000000000000000000";
-
-export const ZERO_BYTES32: string =
-  "0x0000000000000000000000000000000000000000000000000000000000000000";
-
-export const MAX_UINT256: bigint = BigInt(
-  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-);
