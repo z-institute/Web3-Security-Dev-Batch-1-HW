747517a119faaeaa596e810be104a91d29d229c0
alex0207s <alexliao-dev@proton.me>
2024-03-28 00:51:29 +0800

    forge format

--- w6/test/readOnlyReentranct.t.sol
+++ w6/test/readOnlyReentranct.t.sol
@@ -6,16 +6,16 @@ import {ReadOnlyReentranceVul} from "src/example/readOnlyVul.sol";
 import {AttackReadOnlyVul} from "src/example/attackReadOnlyVul.sol";
 
 contract exploitReadOnlyReentrancyTest is Test {
-  AttackReadOnlyVul public attackReadOnlyVul;
-  ReadOnlyReentranceVul public victim;
+    AttackReadOnlyVul public attackReadOnlyVul;
+    ReadOnlyReentranceVul public victim;
 
-  function setUp() public {
-    victim = new ReadOnlyReentranceVul();
-    attackReadOnlyVul = new AttackReadOnlyVul(address(victim));
-  }
+    function setUp() public {
+        victim = new ReadOnlyReentranceVul();
+        attackReadOnlyVul = new AttackReadOnlyVul(address(victim));
+    }
 
-  function testPwn() public {
-    attackReadOnlyVul.setup{value: 10 ether}();
-    attackReadOnlyVul.pwn{value: 100000 ether}();
-  }
+    function testPwn() public {
+        attackReadOnlyVul.setup{value: 10 ether}();
+        attackReadOnlyVul.pwn{value: 100000 ether}();
+    }
 }
